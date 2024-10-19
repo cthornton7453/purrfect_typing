f0d03356882eb742d79d7e059f7a10e62f8ff729
cthornton7453 <cthornton@booksys.com>
2024-10-19 12:13:37 -0500

    dont need auth store

--- resources/js/Stores/authStore.ts
+++
@@ -1,9 +0,0 @@
-// stores/authStore.ts
-import { defineStore } from 'pinia';
-
-export const useAuthStore = defineStore('auth', {
-    state: () => ({
-        isAuthenticated: false,
-        userName: '',
-    }),
-});
